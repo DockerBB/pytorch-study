fe6d9e0161236b8acf5902eecb3ab6fb54ae8d9c
DockerBB <837555220@qq.com>
2019-11-08 15:36:14 +0800

    chapter 3.11\

--- code/chapter3/chapter3.11.py
+++ code/chapter3/chapter3.11.py
@@ -1,4 +1,5 @@
 import torch
+import torch.utils
 import numpy as np
 from matplotlib import pyplot as plt
 import sys
@@ -27,7 +28,36 @@ def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
 
 num_epochs, loss = 100, torch.nn.MSELoss()
 
-def fit_and_plot(trian_features, test_features, train_labels, test_labels):
-    net = torch.nn.Linear(train_labels.shape[-1],1)
+def fit_and_plot(train_features, test_features, train_labels, test_labels):
+    net = torch.nn.Linear(train_features.shape[-1], 1)
+    # 通过Linear文档可知，pytorch已经将参数初始化了，所以我们这里就不手动初始化了
+
+    batch_size = min(10, train_labels.shape[0])
+    dataset = torch.utils.data.TensorDataset(train_features, train_labels)
+    train_iter = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+    train_ls, test_ls = [], []
+    for _ in range(num_epochs):
+        for X, y in train_iter:
+            l = loss(net(X), y.view(-1, 1))
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+        train_labels = train_labels.view(-1, 1)
+        test_labels = test_labels.view(-1, 1)
+        train_ls.append(loss(net(train_features), train_labels).item())
+        test_ls.append(loss(net(test_features), test_labels).item())
+    print('final epoch: train loss', train_ls[-1], 'test loss', test_ls[-1])
+    semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'loss',
+             range(1, num_epochs + 1), test_ls, ['train', 'test'])
+    print('weight:', net.weight.data,
+          '\nbias:', net.bias.data)
+
+#fit_and_plot(poly_features[:n_train, :], poly_features[n_train:, :], labels[:n_train], labels[n_train:]) # 正常
+#fit_and_plot(features[:n_train, :],features[n_train:, :], labels[:n_train], labels[n_train:]) # 欠拟合
+fit_and_plot(poly_features[0:2, :],poly_features[n_train:, :], labels[0:2], labels[n_train:]) #  过拟合
+
+
 
 

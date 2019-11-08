56741ee214f66783ca6b9a8e9fb5f8b3d77ebad6
DockerBB <837555220@qq.com>
2019-11-08 19:42:55 +0800

    chapter3.12

--- code/chapter3/chapter3.12.py
+++ code/chapter3/chapter3.12.py
@@ -54,3 +54,32 @@ def fit_and_plot(lambd):
 # fit_and_plot(lambd=0) # 过拟合
 # fit_and_plot(lambd=3) # 权重衰减
 
+def fit_and_plot_pytorch(wd):
+    # 对权重参数衰减，权重名称一般以Weight结尾
+    net = nn.Linear(num_inputs, 1)
+    nn.init.normal_(net.weight, mean=0, std=1)
+    nn.init.normal_(net.bias, mean=0, std=1)
+    optimizer_w = torch.optim.SGD(params=[net.weight], lr=lr, weight_decay=wd)# 对权重衰减
+    optimizer_b = torch.optim.SGD(params=[net.bias], lr=lr)
+
+    train_ls, test_ls = [], []
+    for _ in range(num_epochs):
+        for X, y in train_iter:
+            l = loss(net(X), y).mean()
+            optimizer_w.zero_grad()
+            optimizer_b.zero_grad()
+
+            l.backward()
+
+            #对Optimize实例分别调用step函数, 分别更新权重和偏差
+            optimizer_w.step()
+            optimizer_b.step()
+        train_ls.append(loss(net(train_features),train_labels).mean().item())
+        test_ls.append(loss(net(test_features), test_labels).mean().item())
+
+    d2l.semilogy(range(1,num_epochs+1),train_ls,'epochs','loss',
+                 range(1,num_epochs+1),test_ls,['train','test'])
+    print('L2 norm of w：', net.weight.data.norm().item())
+
+# fit_and_plot_pytorch(0) #过拟合
+fit_and_plot_pytorch(3) #权重衰减

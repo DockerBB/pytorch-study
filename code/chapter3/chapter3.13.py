061355c34788dba77be698793b746943ea9af1aa
DockerBB <837555220@qq.com>
2019-11-09 10:42:15 +0800

    chapter3.13

--- code/chapter3/chapter3.13.py
+++ code/chapter3/chapter3.13.py
@@ -14,8 +14,55 @@ def dropout(X, drop_prob):
     if keep_prob == 0:
         return torch.zeros_like(X)
     temp = torch.rand(X.shape)
-    mask = (temp > keep_prob).float()
+    mask = (temp < keep_prob).float()
     return mask * X / keep_prob
 
+#定义模型参数
 
+num_inputs, num_outputs, num_hiddens1, num_hiddens2 = 784, 10, 256, 256
 
+W1 = torch.tensor(np.random.normal(0, 0.01, size=(num_inputs, num_hiddens1)), dtype=torch.float, requires_grad=True)
+b1 = torch.zeros(num_hiddens1, requires_grad=True)
+W2 = torch.tensor(np.random.normal(0,0.01,size=(num_hiddens1,num_hiddens2)),dtype=torch.float, requires_grad=True)
+b2 = torch.zeros(num_hiddens2,requires_grad=True)
+W3 = torch.tensor(np.random.normal(0,0.01,size=(num_hiddens2, num_outputs)), dtype=torch.float, requires_grad=True)
+b3 = torch.zeros(num_outputs, requires_grad=True)
+
+params = [W1, b1, W2, b2, W3, b3]
+
+#定义模型
+drop_prob1, drop_prob2 = 0.2, 0.5
+
+def net(X, is_training=True):
+    X = X.view(-1, num_inputs)
+    H1 = (torch.matmul(X, W1) + b1).relu()
+    if is_training:
+        H1 = dropout(H1, drop_prob1)
+    H2 = (torch.matmul(H1, W2) + b2).relu()
+    if is_training:
+        H2 = dropout(H2, drop_prob2)
+
+    return torch.matmul(H2, W3) + b3
+
+num_epochs, lr, batch_size, = 5, 100.0, 256
+loss = torch.nn.CrossEntropyLoss()
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+#d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, params, lr) 训练和测试模型
+
+#简介实现
+net = nn.Sequential(
+    d2l.FlattenLayer(),
+    nn.Linear(num_inputs, num_hiddens1),
+    nn.ReLU(),
+    nn.Dropout(drop_prob1),
+    nn.Linear(num_hiddens1, num_hiddens2),
+    nn.ReLU(),
+    nn.Dropout(drop_prob2),
+    nn.Linear(num_hiddens2, num_outputs)
+)
+
+for param in net.parameters():
+    nn.init.normal_(param, mean=0, std=0.01)
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.5)
+d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size, None, None, optimizer)

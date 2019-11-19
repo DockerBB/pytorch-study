9b079d77c932de37ed47dbe38c5f323daa31e509
DockerBB <837555220@qq.com>
2019-11-19 17:02:07 +0800

    chapter5.11

--- code/chapter5/chapter5.11.py
+++ code/chapter5/chapter5.11.py
@@ -11,6 +11,7 @@ from torch import nn, optim
 import torch.nn.functional as F
 import os
 import sys
+from torchsummary import summary
 sys.path.append("..")
 import d2lzh_pytorch as d2l
 #多块使用逗号隔开
@@ -35,3 +36,107 @@ class Residual(nn.Module):
         if self.conv3:
             X = self.conv3(X)
         return F.relu(Y + X)
+
+
+# ResNet 模型
+# ResNet的前两层跟之前介绍的GoogLeNet中的一样：在输出通道数为64、步幅为2的7×7卷积层
+# 后接步幅为2的3×3的最大池化层。不同之处在于ResNet每个卷积层后增加的批量归一化层。
+
+net = nn.Sequential(
+    nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+    nn.BatchNorm2d(64),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1) # (n+1)/2
+)
+
+def resnet_block(in_channels, out_channels, num_residuals, first_block=False):
+    if first_block:
+        assert  in_channels == out_channels
+    blk = []
+    for i in range(num_residuals):
+        if i == 0 and not first_block: # 第一个模块使用了步幅为二的最大池化层，所以不需要
+                                       # 减小高和宽。所以第1个模块不会走这个分支，后面的
+                                       # 模块第一层走这个分支，第二层走else分支
+            blk.append(Residual(in_channels, out_channels, use_1x1conv=True, stride=2))
+        else:
+            blk.append(Residual(out_channels, out_channels))
+    return nn.Sequential(*blk)
+
+net.add_module("resnet_block1", resnet_block(64, 64, 2, first_block=True))
+net.add_module("resnet_block2", resnet_block(64, 128, 2))
+net.add_module("resnet_block3", resnet_block(128, 256, 2))
+net.add_module("resnet_block4", resnet_block(256, 512, 2))
+
+net.add_module("global_avg_pool", d2l.GlobalAvgPool2d()) # GlobalAvgPool2d的输出: (Batch, 512, 1, 1)
+net.add_module("fc", nn.Sequential(d2l.FlattenLayer(), nn.Linear(512, 10)))
+
+
+blk = Residual(3, 3, use_1x1conv=True, stride=2)
+summary(blk, (3, 7, 7))
+
+summary(net, (1, 224, 224))
+X = torch.rand((1, 1, 224, 224))
+for name, layer in net.named_children():
+    X = layer(X)
+    print(name, ' output shape:\t', X.shape)
+
+batch_size = 256
+# 如出现“out of memory”的报错信息，可减小batch_size或resize
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=48)
+
+lr, num_epochs = 0.001, 5
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
+d2l.train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
